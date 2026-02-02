e27c17620c6fb916cd284aa2ab6763dfe198c3ab
Rokuo <alexandre.demeester@epitech.eu>
2026-02-02 21:46:50 +0100

    Add: Made random article picker from flutter doc

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'package:manga_base/game.dart';
+import 'package:http/http.dart';
+
+import 'summary.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,138 +15,148 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar (
-          title: Align(
-            alignment: Alignment.centerLeft,
-            child: Text('birdle'),
-          ),
-        ),
-        body: Center(
-          child: GamePage(),
-        ),
+      home: ArticleView()
+    );
+  }
+}
+
+class ArticleModel {
+  Future<Summary> getRandomArticleSummary() async {
+    final uri = Uri.https(
+      'en.wikipedia.org',
+      '/api/rest_v1/page/random/summary',
+    );
+    final response = await get(uri);
+
+    if (response.statusCode != 200) {
+      throw HttpException('Failed to update resource');
+    }
+    return Summary.fromJson(jsonDecode(response.body));
+  }
+}
+
+class ArticleViewModel extends ChangeNotifier {
+  final ArticleModel model;
+  Summary? summary;
+  String? errorMessage;
+  bool loading = false;
+
+  ArticleViewModel(this.model) {
+    getRandomArticleSummary();
+  }
+
+  Future<void> getRandomArticleSummary() async {
+    loading = true;
+    notifyListeners();
+
+    try {
+      summary = await model.getRandomArticleSummary();
+      print('Article loaded: ${summary!.titles.normalized}');
+      errorMessage = null;
+    } on HttpException catch (error) {
+      print('Error loading the arcticle: ${error.message}');
+      errorMessage = error.message;
+      summary = null;
+    }
+
+    loading = false;
+    notifyListeners();
+  }
+}
+
+class ArticleView extends StatelessWidget {
+  ArticleView({super.key});
+
+  final ArticleViewModel viewModel = ArticleViewModel(ArticleModel());
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Wikipedia Flutter'),
+        actions: [],
+      ),
+      body: ListenableBuilder(
+        listenable: viewModel,
+        builder: (context, child) {
+          return switch ((
+            viewModel.loading,
+            viewModel.summary,
+            viewModel.errorMessage,
+          )) {
+            (true, _, _) => CircularProgressIndicator(),
+            (false, _, String message) => Center(child: Text(message)),
+            (false, null, null) => Center(
+              child: Text('An unknown error has occurred'),
+            ),
+            // The summary must be non-null in this switch case.
+            (false, Summary summary, null) => ArticlePage(
+              summary: summary,
+              onPressed: viewModel.getRandomArticleSummary,
+            ),
+          };
+        },
       ),
     );
   }
 }
 
-class Tile extends StatelessWidget {
-  const Tile (this.letter, this.hitType, {super.key});
+class ArticlePage extends StatelessWidget {
+  const ArticlePage({
+    super.key,
+    required this.summary,
+    required this.onPressed,
+  });
 
-  final String letter;
-  final HitType hitType;
+  final Summary summary;
+  final VoidCallback onPressed;
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedContainer(
-      duration: Duration(milliseconds: 500),
-      curve: Curves.bounceIn,
-      width: 60,
-      height: 60,
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.grey.shade300),
-        color: switch (hitType) {
-          HitType.hit => Colors.green,
-          HitType.partial => Colors.yellow,
-          HitType.miss => Colors.grey,
-          _ => Colors.white,
-        },
-      ),
-      child: Center(
-        child: Text(
-          letter.toUpperCase(),
-          style: Theme.of(context).textTheme.titleLarge,
-        ),
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          ArticleWidget(
+              summary: summary,
+          ),
+          ElevatedButton(
+            onPressed: onPressed,
+            child: Text('Next random article'),
+          ),
+        ],
       ),
     );
-  } 
+  }
 }
 
-class _GamePageState extends State<GamePage> {
-  final Game _game = Game();
+class ArticleWidget extends StatelessWidget {
+  const ArticleWidget({super.key, required this.summary});
+
+  final Summary summary;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Column(
-        spacing: 5.0,
+        spacing: 10.0,
         children: [
-          for (var guess in _game.guesses)
-            Row(
-              spacing: 5.0,
-              children: [
-                for (var letter in guess)
-                  Tile(letter.char, letter.type)
-              ],
+          if (summary.hasImage)
+            Image.network(
+              summary.originalImage!.source,
             ),
-          GuessInput(
-            onSubmitGuess: (String guess) {
-              setState(() {
-                _game.guess(guess);
-              });
-            }
-          )
-          // ..._game.guesses.map((element) => Row(spacing: 5.0, children: [...element.map((e) => Tile(e.char, e.type),)]))
+            Text(
+              summary.titles.normalized,
+              overflow: TextOverflow.ellipsis,
+              style: TextTheme.of(context).displaySmall,
+            ),
+            Text(
+              summary.extract,
+            )
         ],
       ),
-    );
-  }
-
-}
-
-class GamePage extends StatefulWidget {
-  GamePage({super.key});
-  // This object is part of the game.dart file.
-  // It manages wordle logic, and is outside the scope of this tutorial.
-  @override
-  State<GamePage> createState() => _GamePageState();
-}
-
-class GuessInput extends StatelessWidget {
-  GuessInput({super.key, required this.onSubmitGuess});
-
-  final void Function(String) onSubmitGuess;
-
-  final TextEditingController _textEditingController = TextEditingController();
-  
-  final FocusNode _focusNode = FocusNode();
-
-  void _onSubmit() {
-    onSubmitGuess(_textEditingController.text.trim());
-    _textEditingController.clear();
-    _focusNode.requestFocus();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // You'll build the UI in the next steps.
-
-
-    return Row(
-      children: [
-        Expanded(child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: TextField(
-            maxLength: 5,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.all(Radius.circular(35))
-              )
-            ),
-            controller: _textEditingController,
-            autofocus: true,
-            focusNode: _focusNode,
-            onSubmitted: (String guess) => _onSubmit(),
-          ),
-        )),
-        IconButton(
-          onPressed: _onSubmit,
-          padding: EdgeInsets.zero,
-          icon: Icon(Icons.arrow_circle_up),
-        )
-      ],
     );
   }
 }
---
+++ lib/wordle.main.dart
@@ -0,0 +1,147 @@
+import 'package:flutter/material.dart';
+import 'package:manga_base/game.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar (
+          title: Align(
+            alignment: Alignment.centerLeft,
+            child: Text('birdle'),
+          ),
+        ),
+        body: Center(
+          child: GamePage(),
+        ),
+      ),
+    );
+  }
+}
+
+class Tile extends StatelessWidget {
+  const Tile (this.letter, this.hitType, {super.key});
+
+  final String letter;
+  final HitType hitType;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 500),
+      curve: Curves.bounceIn,
+      width: 60,
+      height: 60,
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey.shade300),
+        color: switch (hitType) {
+          HitType.hit => Colors.green,
+          HitType.partial => Colors.yellow,
+          HitType.miss => Colors.grey,
+          _ => Colors.white,
+        },
+      ),
+      child: Center(
+        child: Text(
+          letter.toUpperCase(),
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
+      ),
+    );
+  } 
+}
+
+class _GamePageState extends State<GamePage> {
+  final Game _game = Game();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        spacing: 5.0,
+        children: [
+          for (var guess in _game.guesses)
+            Row(
+              spacing: 5.0,
+              children: [
+                for (var letter in guess)
+                  Tile(letter.char, letter.type)
+              ],
+            ),
+          GuessInput(
+            onSubmitGuess: (String guess) {
+              setState(() {
+                _game.guess(guess);
+              });
+            }
+          )
+          // ..._game.guesses.map((element) => Row(spacing: 5.0, children: [...element.map((e) => Tile(e.char, e.type),)]))
+        ],
+      ),
+    );
+  }
+
+}
+
+class GamePage extends StatefulWidget {
+  GamePage({super.key});
+  // This object is part of the game.dart file.
+  // It manages wordle logic, and is outside the scope of this tutorial.
+  @override
+  State<GamePage> createState() => _GamePageState();
+}
+
+class GuessInput extends StatelessWidget {
+  GuessInput({super.key, required this.onSubmitGuess});
+
+  final void Function(String) onSubmitGuess;
+
+  final TextEditingController _textEditingController = TextEditingController();
+  
+  final FocusNode _focusNode = FocusNode();
+
+  void _onSubmit() {
+    onSubmitGuess(_textEditingController.text.trim());
+    _textEditingController.clear();
+    _focusNode.requestFocus();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // You'll build the UI in the next steps.
+
+
+    return Row(
+      children: [
+        Expanded(child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextField(
+            maxLength: 5,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(35))
+              )
+            ),
+            controller: _textEditingController,
+            autofocus: true,
+            focusNode: _focusNode,
+            onSubmitted: (String guess) => _onSubmit(),
+          ),
+        )),
+        IconButton(
+          onPressed: _onSubmit,
+          padding: EdgeInsets.zero,
+          icon: Icon(Icons.arrow_circle_up),
+        )
+      ],
+    );
+  }
+}
