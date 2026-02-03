fd63b737c0311b3939197a0be69c561e43e12a49
Rokuo <alexandre.demeester@epitech.eu>
2026-02-04 00:37:23 +0100

    Add: started adaptive layout learnin

--- lib/main.dart
+++ lib/main.dart
@@ -1,162 +1,27 @@
-import 'dart:convert';
-import 'dart:io';
+import 'package:flutter/cupertino.dart';
+import 'package:manga_base/data/contact_group.dart';
+import 'package:manga_base/screens/adaptive_layout.dart';
 
-import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-
-import 'summary.dart';
+final contactGroupsModel = ContactGroupsModel();
 
 void main() {
-  runApp(const MainApp());
+  runApp(const RolodexApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class RolodexApp extends StatelessWidget {
+  const RolodexApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-
-    return MaterialApp(
-      home: ArticleView()
-    );
-  }
-}
-
-class ArticleModel {
-  Future<Summary> getRandomArticleSummary() async {
-    final uri = Uri.https(
-      'en.wikipedia.org',
-      '/api/rest_v1/page/random/summary',
-    );
-    final response = await get(uri);
-
-    if (response.statusCode != 200) {
-      throw HttpException('Failed to update resource');
-    }
-    return Summary.fromJson(jsonDecode(response.body));
-  }
-}
-
-class ArticleViewModel extends ChangeNotifier {
-  final ArticleModel model;
-  Summary? summary;
-  String? errorMessage;
-  bool loading = false;
-
-  ArticleViewModel(this.model) {
-    getRandomArticleSummary();
-  }
-
-  Future<void> getRandomArticleSummary() async {
-    loading = true;
-    notifyListeners();
-
-    try {
-      summary = await model.getRandomArticleSummary();
-      print('Article loaded: ${summary!.titles.normalized}');
-      errorMessage = null;
-    } on HttpException catch (error) {
-      print('Error loading the arcticle: ${error.message}');
-      errorMessage = error.message;
-      summary = null;
-    }
-
-    loading = false;
-    notifyListeners();
-  }
-}
-
-class ArticleView extends StatelessWidget {
-  ArticleView({super.key});
-
-  final ArticleViewModel viewModel = ArticleViewModel(ArticleModel());
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Wikipedia Flutter'),
-        actions: [],
+    return CupertinoApp(
+      title: 'Rolodex',
+      theme: const CupertinoThemeData(
+        barBackgroundColor: CupertinoDynamicColor.withBrightness(
+          color: Color(0xFFF9F9F9),
+          darkColor: Color(0xFF1D1D1D),
+        ),
       ),
-      body: ListenableBuilder(
-        listenable: viewModel,
-        builder: (context, child) {
-          return switch ((
-            viewModel.loading,
-            viewModel.summary,
-            viewModel.errorMessage,
-          )) {
-            (true, _, _) => CircularProgressIndicator(),
-            (false, _, String message) => Center(child: Text(message)),
-            (false, null, null) => Center(
-              child: Text('An unknown error has occurred'),
-            ),
-            // The summary must be non-null in this switch case.
-            (false, Summary summary, null) => ArticlePage(
-              summary: summary,
-              onPressed: viewModel.getRandomArticleSummary,
-            ),
-          };
-        },
-      ),
-    );
-  }
-}
-
-class ArticlePage extends StatelessWidget {
-  const ArticlePage({
-    super.key,
-    required this.summary,
-    required this.onPressed,
-  });
-
-  final Summary summary;
-  final VoidCallback onPressed;
-
-  @override
-  Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          ArticleWidget(
-              summary: summary,
-          ),
-          ElevatedButton(
-            onPressed: onPressed,
-            child: Text('Next random article'),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class ArticleWidget extends StatelessWidget {
-  const ArticleWidget({super.key, required this.summary});
-
-  final Summary summary;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Column(
-        spacing: 10.0,
-        children: [
-          if (summary.hasImage)
-            Image.network(
-              summary.originalImage!.source,
-            ),
-            Text(
-              summary.titles.normalized,
-              overflow: TextOverflow.ellipsis,
-              style: TextTheme.of(context).displaySmall,
-            ),
-            Text(
-              summary.extract,
-            )
-        ],
-      ),
+      home: AdaptiveLayout(),
     );
   }
 }
