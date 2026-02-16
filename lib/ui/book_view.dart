4958d32a94b5ce6ba993a38c6050e8bbfc0069ee
Rokuo <alexandre.demeester@epitech.eu>
2026-02-16 01:36:09 +0100

    Add: book details view added information

--- lib/ui/book_view.dart
+++ lib/ui/book_view.dart
@@ -13,9 +13,19 @@ class BookModalView extends StatelessWidget {
       ShadDialog(
         title: Text(book.title),
         description: Text(book.authors.toString()),
+        scrollable: true,
+        alignment: Alignment.center,
+        radius: BorderRadius.all(Radius.elliptical(10, 100)),
+        padding: EdgeInsets.symmetric(horizontal: 24, vertical: 0),
         child: Column(
           children: [
+            Text('Date de parution: ${book.firstPublishYear}'),
+            Text('Nombre de pages: ${book.pageCount}'),
             Image.network(book.coverUrl!),
+            Center(child : Row(children: [
+              for (String subject in book.subjects)
+                ShadBadge.secondary(child: Text(subject)),
+            ])),
             Text(book.description!)
           ],
         ),
